3d969aa512d8408ff968cc8466245fa3d80bc9a7
Juan Cunez <juan.cunez@partiir.com>
2025-03-07 14:45:54 -0500

    Mejoras en la forma de descarga

--- downloader.py
+++ downloader.py
@@ -1,47 +1,29 @@
-#import os
-
-#with open('links.txt', 'r', encoding='utf-8') as file:
-#    music_list = file.readlines()
-#music_list = list(set(music_list))
-
-#for indice,  music in enumerate(music_list):
-#    os.system("yt-dlp.exe '" + music + "' -f " + 'best[height<1025]'+ "  --no-playlist  -r 30M --buffer-size 16k --windows-filenames  --write-thumbnail  --write-subs
-#    --extract-audio --audio-format mp3 --keep-video -S res,ext:mp4:m4a")
-
-
 import os
 from concurrent.futures import ThreadPoolExecutor
 import subprocess
+import json
 
 def descargar_video(music):
-    
-    #os.system(
-    #    "yt-dlp.exe " + music + " -f " + 'best[height<1025]+bestvideo[ext=mp4]+bestaudio/best[ext=mp4]/best' +
-    #    " --no-playlist -r 30M --buffer-size 16k --windows-filenames --write-thumbnail --write-subs --extract-audio --audio-format mp3 --keep-video -S res,ext:mp4:m4a"
-    #)
+    # Obtener el artista y el nombre de la música
+    artist = music['Artist']
+    name = music['Name']
+    video_url = music['link']
 
-    output_template = "%(title)s [%(id)s].%(ext)s"
+    # Formato de salida
+    output_template = f"{artist} - {name}.mp3"
 
-    output_filename = subprocess.check_output([
-        "yt-dlp2025.exe", "--get-filename", "-o", output_template, music
-    ]).decode("utf-8").strip()
-
-    print("Trying to download: " +output_filename)
-
-    if not os.path.exists(output_filename):
-        print("Downloading: " + output_filename)
-        os.system(
-            "yt-dlp2025.exe " + music + " -f " + 'best[height<1025]+bestvideo[ext=mp4]+bestaudio/best[ext=mp4]/best' +
-            " --no-playlist -r 30M --buffer-size 16k --extract-audio --audio-format mp3 --keep-video -S res,ext:mp4:m4a"
-        )
-    else:
-        print("File already exists: " + output_filename)
-
+    subprocess.run([
+        "yt-dlp2025.exe", video_url,
+        "-f", "bestaudio",
+        "--extract-audio",
+        "--audio-format", "mp3",
+        "--output", output_template,
+        "--no-playlist"
+    ])
 
 if __name__ == "__main__":
-    with open('links.txt', 'r', encoding='utf-8') as file:
-        music_list = file.readlines()
-    music_list = list(set(music_list))
+    with open('links.json', 'r', encoding='utf-8') as file:
+        music_list = json.load(file)
 
     # Número de descargas a ejecutar simultáneamente
     num_descargas_simultaneas = 100
@@ -50,15 +32,3 @@ if __name__ == "__main__":
     with ThreadPoolExecutor(max_workers=num_descargas_simultaneas) as executor:
         # Lanzar las funciones en paralelo
         executor.map(descargar_video, music_list)
-
-
-
-
-
-
-# -f "best[height=720]" --no-playlist -r 10M --buffer-size 16k --windows-filenames  --write-thumbnail  --write-subs  --extract-audio --audio-format mp3 --keep-video -S res,ext:mp4:m4a
-
-
-
-#yt-dlp.exe "https://www.youtube.com/watch?v=LHaOt4mdecI&pp=ygUZWm9kaWFjbyBNb2RlcmF0dG8gb2ZpY2lhbA%3D%3D" -f "best[height<1025]" --no-playlist  -r 30M --buffer-size 16k
-# --windows-filenames  --write-thumbnail  --write-subs  --extract-audio --audio-format mp3 --keep-video -S res,ext:mp4:m4a
--- main.py
+++ main.py
@@ -2,54 +2,53 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import json
 from selenium.webdriver.chrome.service import Service
+import json
 
-from selenium.webdriver.support.wait import WebDriverWait
-
+# Especificar la ruta al controlador de Chrome
 chrome_driver_path = "C:\\chromedriver.exe"
-prefs = {"download.default_directory" : "I:\\Videos"}
 
-# Leer el archivo musics.txt
-with open('musics.txt', 'r', encoding='utf-8') as file:
-    music_list = file.readlines()
-
-
-# Specify the path to the Chrome driver
-chrome_driver_path = 'C:\\chromedriver.exe'  # update with your actual path
-
-# Create a Service object
+# Crear un objeto Service
 service = Service(chrome_driver_path)
 
-# Create the WebDriver instance using the Service
+# Crear la instancia de WebDriver usando el Service
 driver = webdriver.Chrome(service=service)
 
+# Leer el archivo musics.json
+with open('musics.json', 'r', encoding='utf-8') as file:
+    music_list = json.load(file)
+
+# Lista para almacenar los resultados
+results = []
 
 driver.get('https://www.youtube.com')
 time.sleep(5)
 
-# Iterar sobre cada línea en musics.txt
-for indice,  music in enumerate(music_list):
+# Iterar sobre cada elemento en music_list
+for indice, music in enumerate(music_list):
     try:
         search_box = driver.find_element('name', 'search_query')
         search_box.clear()
-        search_box.send_keys(music + Keys.RETURN)
+        search_box.send_keys(music['Name'] + " " + music['Artist'] + Keys.RETURN)
         time.sleep(2)
 
-        first_result = driver.find_elements(By.CSS_SELECTOR ,'.ytd-section-list-renderer #contents #dismissible a#thumbnail')
+        first_result = driver.find_elements(By.CSS_SELECTOR, '.ytd-section-list-renderer #contents #dismissible a#thumbnail')
         video_url = first_result[0].get_attribute('href')
 
         # Almacenar el resultado en la lista en formato JSON
-        #result_entry = {"name": music.strip(), "link": video_url}
-        #results.append(result_entry)
-        with open("links.txt", "a") as temp_file:
-            temp_file.write("%s\n" % video_url)
-    except:
-        print("music failed: " + str(indice) + " " +  music)
+        result_entry = {
+            "Artist": music['Artist'],
+            "Name": music['Name'],
+            "link": video_url
+        }
+        print(result_entry)
+        results.append(result_entry)
+
+    except Exception as e:
+        print(f"Music failed: {indice} {music['Name']} - Error: {str(e)}")
 
 # Guardar la lista de resultados en formato JSON en el archivo links.json
-#with open('links.json', 'w') as json_file:
-    #json.dump(results, json_file, indent=2)
+with open('links.json', 'w', encoding='utf-8') as json_file:
+    json.dump(results, json_file, ensure_ascii=False, indent=2)
 
 driver.quit()
